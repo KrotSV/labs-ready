0b2b95a7f0467f42cf407a5b7b021a52f7e7f468
Sergei_Krotov1 <sergei_krotov1@epam.com>
2017-10-19 13:25:13 +0300

    start 5

--- lab-5-aop/src/main/java/lab/aop/Politeness.java
+++ lab-5-aop/src/main/java/lab/aop/Politeness.java
@@ -16,36 +16,4 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class Politeness {
 
-  @Pointcut("execution(* sellSquishee(..))")
-  public void sellSquishee() {
-  }
-
-  @Before("sellSquishee()")
-  public void sayHello(JoinPoint joinPiont) {
-    AopLog.append(
-        "Hello " + ((Customer) joinPiont.getArgs()[0]).getName() + ". How are you doing? \n");
-  }
-
-  @AfterReturning(pointcut = "sellSquishee()", returning = "retVal", argNames = "retVal")
-  public void askOpinion(Squishee retVal) {
-    AopLog.append("Is " + retVal.getName() + " Good Enough? \n");
-  }
-
-  @AfterThrowing(pointcut = "sellSquishee()", throwing = "e")
-  public void sayYouAreNotAllowed(CustomerBrokenException e) {
-    AopLog.append("Hmmm... \n");
-  }
-
-  @After("sellSquishee()")
-  public void sayGoodBye() {
-    AopLog.append("Good Bye! \n");
-  }
-
-  @Around("sellSquishee()")
-  public Object sayPoliteWordsAndSell(ProceedingJoinPoint pjp) throws Throwable {
-    AopLog.append("Hi! \n");
-    Object retVal = pjp.proceed();
-    AopLog.append("See you! \n");
-    return retVal;
-  }
 }
